437799e9a23a19e87585216690664186f27de91e
Willian Souza <91621589+Williansouzh@users.noreply.github.com>
2022-02-21 21:35:48 -0300

    animações

---
+++ SCRIPT/script.js
@@ -0,0 +1,17 @@
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll(){
+    const pageTop = window.pageYOffset +(window.innerHeight * 3 / 4);
+    target.forEach((e)=>{
+        if((pageTop)>e.offsetTop){
+            e.classList.add(animationClass);
+        }
+    })
+};
+animeScroll();
+if(target.length){
+    window.addEventListener('scroll', ()=>{
+        animeScroll()
+    })
+}
